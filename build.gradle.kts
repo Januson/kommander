24636d75823685bfa3e0821fe9aaa70a022333fd
Ondrej Janus <janusonx@gmail.com>
2019-06-08 14:42:16 +0200

    Create a new Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.31"
+}
+
+group = "org.kommander"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.3.2"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.4.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
